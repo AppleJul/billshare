c5a5e0c3eb4c532de1eb46c557d720f41691a0d5
Jul_Dvortsova <dvortsovajula@gmail.com>
2023-08-15 17:41:03 +0200

    Initial Commit

---
+++ billshare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  billshare
+//
+//  Created by Bubble Waffle on 15.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
